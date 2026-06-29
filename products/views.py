5a83114472c5c2cc9d7bc5e7c374a6ff70ec61f3
Alexandre <alexandre@email,com>
2026-06-29 20:06:29 -0300

    Feat: Add global pagination and filtering backends for products

--- products/views.py
+++ products/views.py
@@ -1,11 +1,25 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Products
 from .serializers import ProductSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     """
-    A simple ViewSet for viewing and editing product instances.
-    Provides automatic CRUD actions mapped to standard HTTP methods.
+    A ViewSet for viewing and editing product instances.
+    Includes built-in support for filtering, full-text search, and ordering.
     """
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
+    
+    # Configure filtering backends specifically for this view
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    
+    # Exact match filters
+    filterset_fields = ['stock']
+    
+    # Text search fields (maps to HTTP query param '?search=')
+    search_fields = ['name', 'description']
+    
+    # Order parameters (maps to HTTP query param '?ordering=')
+    ordering_fields = ['price', 'stock']
--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
-    'rest_framework'
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Django REST Framework global configurations
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
