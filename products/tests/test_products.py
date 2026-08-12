05f1fab6cbf8573b6871c1d0e8345229bfd65068
Alexandre <alexandre@email,com>
2026-08-12 15:28:45 -0300

    chore: add Docker PostgreSQL and test setup

--- products/tests/test_products.py
+++ products/tests/test_products.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Products, Category
+from products.models import Products, Category
 
 class ProductAPITests(APITestCase):
 
